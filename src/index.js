37816cb3c26b94b94ba9d0cc1021243ebe1755eb
Stevie Robinson <stevie.robinson@gmail.com>
2025-07-01 22:39:22 +0200

    add units toggle functionality

--- src/index.js
+++ src/index.js
@@ -4,18 +4,38 @@ import { getWeatherForLocation } from './weatherApi';
 import { displayWeather, displayError } from './displayWeather';
 
 let units = 'metric';
+let currentLocation = '';
 
 searchWeather("Jirnsum");
 
 const searchForm = document.querySelector('#searchForm');
 const searchInput = document.getElementById('searchInput');
+const celciusButton = document.querySelector('#celciusButton');
+const farenheitButton = document.querySelector('#fahrenheitButton');
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   searchWeather(searchInput.value);
 })
 
+
+celciusButton.addEventListener('click', () => {
+  units = 'metric';
+  celciusButton.classList.add('active');
+  farenheitButton.classList.remove('active');
+  searchWeather(currentLocation);
+})
+
+farenheitButton.addEventListener('click', () => {
+  units = 'us';
+  celciusButton.classList.remove('active');
+  farenheitButton.classList.add ('active');
+  searchWeather(currentLocation);
+})
+
+
 async function searchWeather(location) {
+  currentLocation = location;
   try {
     let weather = await getWeatherForLocation(location, units);
     displayWeather(weather);
